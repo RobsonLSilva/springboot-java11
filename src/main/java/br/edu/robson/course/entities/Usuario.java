63cfad37d7ba79d4314057a3c9899f7046ecde41
Robson Luiz <robson.lsilva36@gmail.com>
2022-07-02 12:49:11 -0300

    Service layer, component registration

--- src/main/java/br/edu/robson/course/entities/Usuario.java
+++ src/main/java/br/edu/robson/course/entities/Usuario.java
@@ -14,7 +14,7 @@ public class Usuario implements Serializable {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+	private Long id;
 	private String name;
 	private String email;
 	private String phone;
@@ -23,7 +23,7 @@ public class Usuario implements Serializable {
 	public Usuario() {
 	}
 
-	public Usuario(Integer id, String name, String email, String phone, String password) {
+	public Usuario(Long id, String name, String email, String phone, String password) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
@@ -31,11 +31,11 @@ public class Usuario implements Serializable {
 		this.password = password;
 	}
 
-	public Integer getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
--- src/main/java/br/edu/robson/course/resources/UsuarioResource.java
+++ src/main/java/br/edu/robson/course/resources/UsuarioResource.java
@@ -1,21 +1,36 @@
 package br.edu.robson.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.robson.course.entities.Usuario;
+import br.edu.robson.course.services.UsuarioService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UsuarioResource {
 	
+	@Autowired
+	private UsuarioService service;
+	
 	@GetMapping
-	public ResponseEntity<Usuario> findAll(){
+	public ResponseEntity<List<Usuario>> findAll(){
+	
+		List<Usuario> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 		
-		Usuario u = new Usuario(1, "Maria", "maria@gmail.com", "999999999", "12345");
-		return ResponseEntity.ok().body(u);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Usuario> findById(@PathVariable Long id){
+		Usuario obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 		
 	}
 
