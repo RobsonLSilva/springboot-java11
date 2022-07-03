93ba5930ffc0946fbe07e482da7dd63053d984a0
Robson Luiz <robson.lsilva36@gmail.com>
2022-07-03 18:59:48 -0300

    Order, Instant, ISO 8601_02

--- src/main/java/br/edu/robson/course/resources/OrderResource.java
+++ src/main/java/br/edu/robson/course/resources/OrderResource.java
@@ -1,10 +1,37 @@
 package br.edu.robson.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.edu.robson.course.entities.Order;
+import br.edu.robson.course.services.OrderService;
+
 @RestController
-@RequestMapping(value = "/order")
+@RequestMapping(value = "/orders")
 public class OrderResource {
+	
+	@Autowired
+	private OrderService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Order>> findAll(){
+		
+		List<Order> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+		
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Order> findById(@PathVariable Long id){
+		Order obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+		
+	}
 
 }
---
+++ src/main/java/br/edu/robson/course/services/OrderService.java
@@ -0,0 +1,26 @@
+package br.edu.robson.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.edu.robson.course.entities.Order;
+import br.edu.robson.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id){
+		Optional<Order> obj = repository.findById(id);
+		 return	obj.get();
+	}
+}
