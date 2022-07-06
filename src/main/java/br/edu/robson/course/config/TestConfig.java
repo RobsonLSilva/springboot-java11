12bacda32b0134a3ac00a24b477badd4c940e097
Robson Luiz <robson.lsilva36@gmail.com>
2022-07-05 21:06:20 -0300

    Payment, one-to-one association

--- src/main/java/br/edu/robson/course/config/TestConfig.java
+++ src/main/java/br/edu/robson/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import br.edu.robson.course.entities.Category;
 import br.edu.robson.course.entities.Order;
 import br.edu.robson.course.entities.OrderItem;
+import br.edu.robson.course.entities.Payment;
 import br.edu.robson.course.entities.Product;
 import br.edu.robson.course.entities.User;
 import br.edu.robson.course.entities.enums.OrderStatus;
@@ -79,5 +80,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
