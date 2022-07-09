726e744e048c9961c93b8cac87b821adb723283e
Robson Luiz <robson.lsilva36@gmail.com>
2022-07-08 22:34:43 -0300

    Exception handling - delete

---
+++ src/main/java/br/edu/robson/course/services/exceptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package br.edu.robson.course.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+}
